7529c5b101149b6711bf54cac803a0e39de64cb6
Benyamin <konstanb@post.bgu.ac.il>
2024-07-02 16:53:30 +0300

    init

---
+++ Dungeons and Dragons/Dungeons and Dragons/Backend/Tile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dungeons_and_Dragons.Backend
+{
+    internal abstract class Tile
+    {
+        Char tile { get; set; }
+        int PositionX { get; set; }
+        int PositionY { get; set; }
+    }
+}
