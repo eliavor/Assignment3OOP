4d836c4ea5f63373a567ce0afb38fd206ddc2fcb
eliavor <eliavor@post.bgu.ac.il>
2024-07-03 23:21:51 +0300

    view +controller inital classes

--- Dungeons and Dragons/src/Controller/Controller.java
+++ Dungeons and Dragons/src/Controller/Controller.java
@@ -1,19 +1,32 @@
 package Controller;
-
+import java.util.Scanner;
+import Model.board.Board;
 import utilsGeneral.MessageCallBackToController;
 
 public class Controller {
-
+    Board b;
+    Scanner sc = new Scanner(System.in);
     private MessageCallBackToController messageCallBackViewController;
 
     public Controller() {
         messageCallBackViewController = this::nextTick;
     }
+    public void setB(Board b){
+        this.b =b;
+    }
 
     public void nextTick(){
-        //when the method is called, the next tick begins. no need for the message.
+        handleUserInput();
     }
-    public MessageCallBackToController getNessageCallback(){
+    private void handleUserInput(){
+        String userInput = sc.nextLine();
+        while (userInput.length() != 1){
+            userInput = sc.nextLine();
+        }
+        b.handleUserInput(userInput.charAt(0));
+    }
+
+    public MessageCallBackToController getMessageCallback(){
         return messageCallBackViewController;
     }
 }
--- Dungeons and Dragons/src/View/commendLineView/CLI.java
+++ Dungeons and Dragons/src/View/commendLineView/CLI.java
@@ -2,18 +2,20 @@ package View.commendLineView;
 
 import utilsGeneral.MessageCallBackToController;
 import View.utils.View;
+import View.utils.ViewUtils;
 import utilsGeneral.MessageCallBackToView;
-
 import java.util.Dictionary;
 
 public class CLI implements View{
+    private Character[][] board;
+    private String battleString;
+    private String playerString;
 
     private  MessageCallBackToView messageCallBackToView;
 
     private MessageCallBackToController messageCallBackToController;
 
     public CLI(MessageCallBackToController messageCallBackToController){
-
         messageCallBackToView = new MessageCallBackToView() {
 
             @Override
@@ -45,18 +47,32 @@ public class CLI implements View{
     }
 
     private void updateTile(char c, int x, int y){
-
+        board[y][x] = c;
     }
     private void loadMap(int width, int height){
-
+        board = new Character[height][width];
     }
 
-    private void battleInfo(Dictionary<String, String> battleInfo){
-
+    private void battleInfo(Dictionary<String, String> battleInfo1, Dictionary<String, String> battleInfo2 ){
+        if(battleString == null){
+            battleString = "";
+        }
+        battleString += ViewUtils.CreateBattleString(battleInfo1, battleInfo2);
     }
 
     private void playerStats(Dictionary<String, String> playerStats){
+        if(playerString == null){
+            battleString = "";
+        }
+        playerString += ViewUtils.CreatePlayerString(playerStats);
+        printTick();
+    }
 
+    private void printTick(){
+        String boardString = ViewUtils.CreateBoardString(board);
+        String toPrint = battleString +"/n" + boardString + "/n" + playerString;
+        System.out.println(toPrint);
+        messageCallBackToController.nextTick();
     }
 
 
